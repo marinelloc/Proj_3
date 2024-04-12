27fa2771be1d15f826d14648076bb6ab0049337f
AlanBigData <alawrence026@gmail.com>
2024-04-12 16:55:32 -0400

    updated

--- static/js/logic4_ts.js
+++ static/js/logic4_ts.js
@@ -14,27 +14,51 @@ function createMap(evStations) {
     });
   
     L.control.layers(baseMaps, overlayMaps, { collapsed: false }).addTo(map);
-  }
-  
-  // Function to create markers for EV charging stations
-  function createMarkers(evChargers) {
+    
+    // Event listener for clicking on markers
+    evStations.on('click', function(e) {
+        const marker = e.layer;
+        const latlng = marker.getLatLng();
+
+        // Remove any existing circles
+        map.eachLayer(function(layer) {
+            if (layer instanceof L.Circle) {
+                map.removeLayer(layer);
+            }
+        });
+
+        // Create circle with 5-mile radius centered at marker's location
+        const circle = L.circle(latlng, {
+            color: 'red',
+            fillColor: '#f03',
+            fillOpacity: 0.5,
+            radius: 5 * 1609.34 // Convert 5 miles to meters
+        }).addTo(map);
+    });
+}
+
+// Function to create markers for EV charging stations
+function createMarkers(evChargers) {
     const evMarkers = evChargers.map(charger => {
-      const popupContent = `
-      <h3>Station Name: ${charger["station name"]}</h3>
-      <h3>State: ${charger.state}</h3>
-      <h3>City: ${charger.city}</h3>
-      <h3>ZipCode: ${charger.zipcode}</h3>
-      <p>Latitude: ${charger.latitude}</p>
-      <p>Longitude: ${charger.longitude}</p>
-      `;
-      return L.marker([charger.latitude, charger.longitude]).bindPopup(popupContent);
+        const popupContent = `
+            <h3>Station Name: ${charger["station name"]}</h3>
+            <h3>State: ${charger.state}</h3>
+            <h3>City: ${charger.city}</h3>
+            <h3>ZipCode: ${charger.zipcode}</h3>
+            <p>Latitude: ${charger.latitude}</p>
+            <p>Longitude: ${charger.longitude}</p>
+        `;
+        const marker = L.marker([charger.latitude, charger.longitude]).bindPopup(popupContent);
+      
+        return marker;
     });
   
-    createMap(L.layerGroup(evMarkers));
-  }
-  
-  // URL for GeoJSON data
-  const url = "https://marinelloc.github.io/Proj_3/data/tristatecharingstations.json";
-  
-  // Fetch GeoJSON data and create markers when it completes
-  d3.json(url).then(createMarkers);  
+    const evStations = L.layerGroup(evMarkers);
+    createMap(evStations);
+}
+
+// URL for GeoJSON data
+const url = "https://marinelloc.github.io/Proj_3/data/tristatecharingstations.json";
+
+// Fetch GeoJSON data and create markers when it completes
+d3.json(url).then(createMarkers);
